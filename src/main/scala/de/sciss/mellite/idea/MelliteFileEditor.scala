9c2d31c3bd5923b6d3886ce808de3c80f13bea88
Hanns Holger Rutz <contact@sciss.de>
2018-04-15 14:41:31 +0200

    ...

--- src/main/scala/de/sciss/mellite/idea/MelliteFileEditor.scala
+++ src/main/scala/de/sciss/mellite/idea/MelliteFileEditor.scala
@@ -1,6 +1,5 @@
 package de.sciss.mellite.idea
 
-import java.awt.EventQueue
 import java.beans.PropertyChangeListener
 
 import com.intellij.codeHighlighting.BackgroundEditorHighlighter
@@ -10,24 +9,20 @@ import com.intellij.util.EventDispatcher
 import de.sciss.desktop.UndoManager
 import de.sciss.lucre.stm
 import de.sciss.lucre.synth.Sys
-import de.sciss.mellite.gui.FolderView
+import de.sciss.mellite.gui.FolderEditorView
 import de.sciss.synth.proc.Workspace
 import javax.swing.JComponent
-
-import scala.swing.Label
 
 class MelliteFileEditor[S <: Sys[S]]()(implicit ws: Workspace[S])
   extends UserDataHolderBase with FileEditor {
 
-  private[this] val disp = EventDispatcher.create(classOf[PropertyChangeListener])
-
-  private[this] val c = new Label("FOO BAR " + EventQueue.isDispatchThread)
+  private[this] val dispatcher = EventDispatcher.create(classOf[PropertyChangeListener])
 
   private[this] implicit val cursor: stm.Cursor[S] = ws.cursor
 
   private[this] val folderView = cursor.step { implicit tx =>
     implicit val undo: UndoManager = UndoManager()
-    FolderView[S](ws.root)
+    FolderEditorView[S](ws.root)
   }
 
   def getComponent: JComponent = folderView.component.peer
@@ -45,10 +40,10 @@ class MelliteFileEditor[S <: Sys[S]]()(implicit ws: Workspace[S])
   def deselectNotify(): Unit = ()
 
   def addPropertyChangeListener(listener: PropertyChangeListener): Unit =
-    disp.addListener(listener)
+    dispatcher.addListener(listener)
 
   def removePropertyChangeListener(listener: PropertyChangeListener): Unit =
-    disp.removeListener(listener)
+    dispatcher.removeListener(listener)
 
   def getBackgroundHighlighter: BackgroundEditorHighlighter = null
 
--- src/main/scala/de/sciss/mellite/idea/MelliteFileEditorProvider.scala
+++ src/main/scala/de/sciss/mellite/idea/MelliteFileEditorProvider.scala
@@ -3,10 +3,24 @@ package de.sciss.mellite.idea
 import com.intellij.openapi.fileEditor.{FileEditor, FileEditorPolicy, FileEditorProvider}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.WindowManager
+import de.sciss.desktop.WindowHandler
+import de.sciss.desktop.impl.{WindowImpl, WindowStub}
 import de.sciss.file._
 import de.sciss.lucre.stm.store.BerkeleyDB
+import de.sciss.mellite.{Application, Mellite}
 import de.sciss.synth.proc.Workspace
+import javax.swing.UIManager
 
+object MelliteFileEditorProvider {
+  private lazy val _init: Unit = {
+    val laf = UIManager.getLookAndFeel
+    val lafId   = laf.getID
+    if (lafId == "Darcula") UIManager.put("dark-skin", true)
+    Mellite.initTypes()
+    Application.init(Mellite)
+  }
+}
 class MelliteFileEditorProvider extends FileEditorProvider {
   def accept(project: Project, vf: VirtualFile): Boolean = {
 //    val tpe = file.getFileType
@@ -17,11 +31,26 @@ class MelliteFileEditorProvider extends FileEditorProvider {
 
   def createEditor(project: Project, vf: VirtualFile): FileEditor = {
 //    LocalFileSystem.getInstance()
+    MelliteFileEditorProvider._init
     val bdbConfig = BerkeleyDB.Config()
     bdbConfig.allowCreate = false
     val dir = file(vf.getCanonicalPath).parent
     val bdb = BerkeleyDB.factory(dir, bdbConfig)
     val ws  = Workspace.Durable.read(dir, bdb)
+    val frame = WindowManager.getInstance().getFrame(project)
+    val rp    = frame.getRootPane
+    val key   = "de.sciss.desktop.Window" /* WindowImpl.Property */
+    val desktopOk = rp.getClientProperty(key) != null
+    if (!desktopOk) {
+      val value = new WindowStub {
+        protected val delegate: WindowImpl.Delegate =
+          WindowImpl.Delegate.frame(this, frame, hasMenuBar = false, screen = false)
+
+        def handler: WindowHandler = Mellite.windowHandler
+      }
+      rp.putClientProperty(key, value)
+    }
+
     new MelliteFileEditor()(ws)
   }
 
